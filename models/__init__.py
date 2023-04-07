d1dd9d7da84918be69cb706aec86ddce978d8698
Sylvia Jitti <sylviajitti@gmail.com>
2023-04-07 10:49:36 -0700

    Task updated

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""Creates a unique file storage for the app.
-"""
-
-
+"""Initializes the package"""
 from models.engine.file_storage import FileStorage
-
 storage = FileStorage()
 storage.reload()
